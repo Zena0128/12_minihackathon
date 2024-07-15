60e288e5eb2d70a9c41629c01cb70c4166339466
Zena0128 <0128ja@gmail.com>
2024-07-15 12:40:51 +0900

    add:port 5173

--- minihackathon/settings.py
+++ minihackathon/settings.py
@@ -128,7 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "http://127.0.0.1:8000"]
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
